09bf1f6bedfd734b7303a2667ef48dc33383cedc
is264 <dwill.1p@icloud.com>
2025-10-29 21:09:21 +0900

    課題の実装

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import type { MicroCmsPost } from "@/app/_types/MicroCmsPost";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { API_BASE_URL } from "../../_constants/api";
-import type { Post } from "../../_types/Post";
+import { MICROCMS_API_BASE_URL } from "../../_constants/api";
 
 type PostPageProps = {
   params: {
@@ -13,14 +13,18 @@ type PostPageProps = {
 
 export default function PostPage({ params }: PostPageProps) {
   const { id } = params;
-  const [post, setPost] = useState<Post | null>(null);
+  const [post, setPost] = useState<MicroCmsPost>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch(`${API_BASE_URL}/posts/${id}`);
+      const res = await fetch(`${MICROCMS_API_BASE_URL}/posts/${id}`, {
+        headers: {
+          "X-MICROCMS-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY as string,
+        },
+      });
       const data = await res.json();
-      setPost(data.post);
+      setPost(data);
       setIsLoading(false);
     };
     fetcher();
@@ -46,7 +50,7 @@ export default function PostPage({ params }: PostPageProps) {
     <div className="w-[800px] mx-auto my-10 p-4">
       <div>
         <Image
-          src={post.thumbnailUrl}
+          src={post.thumbnail.url}
           alt={post.title}
           width={800}
           height={400}
@@ -58,13 +62,13 @@ export default function PostPage({ params }: PostPageProps) {
             {new Date(post.createdAt).toLocaleDateString()}
           </div>
           <div className="flex justify-end gap-2">
-            {post.categories.map((category: string) => {
+            {post.categories.map((category) => {
               return (
                 <span
-                  key={category}
+                  key={category.id}
                   className="text-xs text-[#0066cc] border border-[#0066cc] rounded-sm px-2 py-1"
                 >
-                  {category}
+                  {category.name}
                 </span>
               );
             })}
